4eca1392885916739517c2b0adf40571e7696440
Michael Skrzypietz <michael.s97@web.de>
2017-10-22 21:02:06 +0200

    updated ProfilServlet

--- WEB-INF/src/ProfilServlet.java
+++ WEB-INF/src/ProfilServlet.java
@@ -7,10 +7,12 @@ import java.nio.file.Paths;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
@@ -30,95 +32,98 @@ public class ProfilServlet extends HttpServlet {
 	private static final String EMAIL_PATTERN =
 			"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
 			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-	private static final String SAVE_DIR = "uploadFiles";
+	private static final String SAVE_DIR = "profilImages";
+	private SettingErrors errors;
 	
 	public ProfilServlet() {
 		super();
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		/*HttpSession session = request.getSession();
+		HttpSession session = request.getSession();
+		this.errors = new SettingErrors();
 		User user = (User) session.getAttribute("user");
 		if (user == null) {
 			System.out.println("User is not set in the session storage.");
 			return;
-		} -> uncomment in production*/ 
-		User user = new User(1); // Delete in production
-		user.setFirstname(request.getParameter("firstName"));
-		user.setLastname(request.getParameter("lastName"));
-		user.setEmail(request.getParameter("email"));
+		} 
 
-		if (isValidInput(user)) {
-			//DAO.updateProfilSettings(user); -> uncomment in production
+		if (isValidInputChange(request)) {
+			user.setFirstname(request.getParameter("firstName"));
+			user.setLastname(request.getParameter("lastName"));
+			user.setEmail(request.getParameter("email"));
+			session.setAttribute("user", user);
+			DAO.updateProfilSettings(user);
 		}
 		
 		if (isPasswordChange(request) && isValidPasswordChange(request)) {
-			//user = (User) session.getAttribute("user"); -> uncomment in production
-			user = new User(2); // Delete in production
-			user.setPwSalt("3"); // Delete in production
-			
+			user = (User) session.getAttribute("user"); 	
 			String pwhash = Hashing.getHashedPassword(request.getParameter("newPassword"), user.getPwSalt());
 			user.setPwHash(pwhash);
 			
-			//DAO.updatePassword(user); -> uncomment in production
-		} else {
-			System.out.println("Invalid password change.");
-		}
+			session.setAttribute("user", user);
+			DAO.updatePassword(user);
+		} 
 		
-		
-		if (isImageUpload(request)) {
+		if (isValidImageUpload(request)) {
 			Part filePart = request.getPart("file");
-		    String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
+			String userFileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
+			String fileType = userFileName.substring(userFileName.indexOf("."), userFileName.length());
+			
+		    String fileName = ((User) request.getSession().getAttribute("user")).getId() + fileType;
 		    
 			String appPath = request.getServletContext().getRealPath("");
-			String savePath = appPath + File.separator + SAVE_DIR;
-	         
-	        File fileSaveDir = new File(savePath);
-	        if (!fileSaveDir.exists()) {
-	            fileSaveDir.mkdir();
-	        }
-	        
+			String savePath = appPath + "img" + File.separator + SAVE_DIR;
+   
 	        filePart.write(savePath + File.separator  + fileName); 
-	        
-	        // user = (User) session.getAttribute("user"); -> uncomment in production
-	        user = new User(1); // delete in production
-	        user.setImgUrl(savePath + File.separator  + fileName);
-	        //DAO.updateImgurl(user); -> uncomment in prodcution
+
+	        user = (User) session.getAttribute("user");
+	        user.setImgUrl(fileName);
+			session.setAttribute("user", user);
+	        DAO.updateImgurl(user);
 		}
- 
+		
+		session.setAttribute("settingErrors", this.errors);
 		response.sendRedirect("jsp/profil.jsp");
 	}
 	
-	private boolean isValidInput(User user) {
-		if (!isValidFirstName(user.getFirstname())) {
+	private boolean isValidInputChange(HttpServletRequest request) {
+		if (!isValidFirstName(request.getParameter("firstName"))) {
+			this.errors.setFirstName("Vornamen dürfen keine Zahlen enthalten und müssen zwischen 2 und 16 Zeichen lang sein.");
 			System.out.println("Profil settings change Error: invalid firstname");
 			return false;
 		}
 		
-		if (!isValidLastName(user.getLastname())) {
+		if (!isValidLastName(request.getParameter("lastName"))) {
+			this.errors.setLastName("Nachnamen dürfen keine Zahlen enthalten und müssen zwischen 2 und 16 Zeichen lang sein.");
 			System.out.println("Profil settings change Error: invalid lastname");
 			return false;
 		}
 		
-		if (!isValidEmail(user.getEmail())) {
+		if (!isValidEmail(request.getParameter("email"))) {
+			this.errors.setEmail("Bitte geben Sie eine korrekte Email-Addresse ein.");
 			System.out.println("Profil settings change Error: invalid email");
 			return false;
 		}
-		/*
-		if (DAO.isEmailTaken(user.getEmail())) {
+
+
+		if (DAO.isEmailTaken(request.getParameter("email"))) {
+			this.errors.setEmail("Die Email ist bereits vergeben.");
 			System.out.println("Profil settings change Error: email is taken");
 			return false;
-		} -> uncomment in production*/
+		}
 		
 		return true;
 	}
 	
 	private boolean isValidFirstName(String firstName) {
-		return firstName != null && firstName.length() > 0 && firstName.length() < 16;
+		return firstName != null && !firstName.matches(".*\\d+.*") &&
+			firstName.length() > 1 && firstName.length() < 16;
 	}
 	
 	private boolean isValidLastName(String lastName) {
-		return lastName != null && lastName.length() > 0 && lastName.length() < 16;
+		return lastName != null && !lastName.matches(".*\\d+.*") &&
+			lastName.length() > 1 && lastName.length() < 16;
 	}
 	
 	private boolean isValidEmail(String email) {
@@ -130,27 +135,33 @@ public class ProfilServlet extends HttpServlet {
 	private boolean isPasswordChange(HttpServletRequest request) {
 		return request.getParameter("currentPassword").length() > 0
 				&& request.getParameter("newPassword").length() > 0
-				&& request.getParameter("newPassword2").length() > 0
-				&& !request.getParameter("currentPassword").equals(
-						request.getParameter("newPassword"));
+				&& request.getParameter("newPassword2").length() > 0;
 	}
 	
 	private boolean isValidPasswordChange(HttpServletRequest request) {
+		User user = (User) request.getSession().getAttribute("user");
 		String newPassword = request.getParameter("newPassword");
-		String newPassword2 = request.getParameter("newPassword2");
+		String newPassword2 = request.getParameter("newPassword2");	
+		String pwhash = Hashing.getHashedPassword(request.getParameter("newPassword"), user.getPwSalt());
+		if (newPassword.equals(newPassword2) && pwhash.equals(user.getPwHash())) return true; 
 		
-		if (newPassword.equals(newPassword2)) return true;
-		
-		System.out.println("Passwords don't match");
+		this.errors.setPassword("Das alte Passwort ist falsch oder die neuen Passwörter stimmen nicht überein.");
+		System.out.println("Invalid password change.");
 		return false;
 	}
 	
-	private boolean isImageUpload(HttpServletRequest request) throws IOException, ServletException {
-		if (request.getPart("file") == null) return false;
+	private boolean isValidImageUpload(HttpServletRequest request) throws IOException, ServletException {
+		if (request.getPart("file") == null) return false; 
 		Part filePart = request.getPart("file");
 	    String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
 	    String fileType = fileName.substring(fileName.indexOf(".") + 1, fileName.length());
-		return !fileName.equals("") && (fileType.equals("png") || fileType.equals("jpeg"));
+
+		boolean result = !fileName.equals("") && (fileType.toUpperCase().equals("PNG") || fileType.toUpperCase().equals("JPEG") || fileType.toUpperCase().equals("JPG"));
+		if (!result && !fileName.equals("")) {
+			this.errors.setProfilImage("Bitte wählen Sie nur png oder jpeg Dateien aus.");
+			System.out.println("Bitte wählen Sie nur png oder jpeg Dateien aus.");
+		}
+		return result;
 	}
 	
 }
