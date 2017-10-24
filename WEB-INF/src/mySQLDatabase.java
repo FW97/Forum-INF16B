30987aa944fa157de34ef91f6f9c072243426896
captainblubb <fabianschulz@hotmail.de>
2017-10-24 19:39:22 +0200

    database

--- WEB-INF/src/mySQLDatabase.java
+++ WEB-INF/src/mySQLDatabase.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:forum/WEB-INF/classes/mySQLDatabase.java
 import java.sql.*;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -34,7 +33,7 @@ import java.sql.DriverManager;
 
 
 
-public class MySQLDatabase
+public class mySQLDatabase
 {
     
 
@@ -43,10 +42,10 @@ public class MySQLDatabase
     private static String dbUser = "root";
     private static String dbPass = "root";
   
-    private static MySQLDatabase mySQLDatabase = new MySQLDatabase(dbURL, dbUser, dbPass);
+    private static mySQLDatabase mySQLDatabase = new mySQLDatabase(dbURL, dbUser, dbPass);
     
     
-    private MySQLDatabase(String dbURL, String dbUser, String dbPass)
+    private mySQLDatabase(String dbURL, String dbUser, String dbPass)
     {
         
         this.dbURL = dbURL;
@@ -68,7 +67,7 @@ public class MySQLDatabase
     /*
      This method returns the instance of the class.
      */
-    public static MySQLDatabase getInstance()
+    public static mySQLDatabase getInstance()
     {
         return mySQLDatabase;
     }
@@ -86,82 +85,3 @@ public class MySQLDatabase
     }
     
 }
-=======
-package de.dhbw.StudentForum;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.Collections;
-
-
-
-/*
- *     Project name:
- *     Assignment:
- *     Author:              Fabian, Janick, Daniel, Andreas
- *     Student number:
- *     Source  file:///Users/fabian/Downloads/MySQLDatabase.java.html 5.10.2017
- *
- *     Description:    Used to create connection to the database.
- *
- *                     Singleton Database class
- *                     Only one instance can be instatiatet.
- */
-
-
-
-public class MySQLDatabase
-{
-    
-    
-    private static MySQLDatabase mySQLDatabase; //Wird im Konstruktor gesetzt davor direkt initialisiert // festcodiert
-    
-    private Connection connection;
-    private String dbURL;
-    private String dbUser;
-    private String dbPass;
-    
-    
-    private MySQLDatabase(String dbURL, String dbUser, String dbPass, String DatabaseUrl, String rootUser, String rootPw)
-    {
-        
-        this.dbURL = dbURL;
-        this.dbUser = dbUser;
-        this.dbPass = dbPass;
-        
-        //Driver init
-        try
-        {
-            // Edit Driverinfo
-            Class.forName("com.mysql.jdbc.Driver");
-            
-            mySQLDatabase = new MySQLDatabase(DatabaseUrl, rootUser, rootPw);
-        } catch (Exception e){
-            e.printStackTrace();}
-        
-    }
-    
-    /*
-     This method returns the instance of the class.
-     */
-    public static MySQLDatabase getInstance()
-    {
-        return mySQLDatabase;
-    }
-    
-    /*
-     Creates and returns a connection to MySQL.
-     */
-    public Connection getConnection()
-    {
-        try
-        {
-            this.connection = DriverManager.getConnection(this.dbURL, this.dbUser, this.dbPass);
-        }catch (Exception e){e.printStackTrace();}
-        return this.connection;
-    }
-    
-}
->>>>>>> 4187448307b1accd0f1d1dd5e04b61c9417fbd8a:WEB-INF/src/mySQLDatabase.java
